e50f1822920043c23f546764b6593abe348bc211
joko26 <playerone99@outlook.de>
2025-05-07 18:39:12 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [max, setMax] = useState(0);
+  const dice = [
+    {value: 4, label: 'D3'},
+    {value: 5, label: 'D4'},
+    {value: 6, label: 'D5'},
+    {value: 7, label: 'D6'},
+    {value: 8, label: 'D7'},
+    {value: 9, label: 'D8'},
+    {value: 11, label: 'D10'},
+    {value: 21, label: 'D20'},
+  ]
+
+  const generateNumber = () => {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(max);
+    setNumber(Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Würfel</h1>
+      <select options={dice} onChange={(e) => setMax(e.target.value)}>
+        {dice.map((dice) => (
+          <option key={dice.value} value={dice.value}>
+            {dice.label}
+          </option>
+        ))}
+      </select>
+      <button onClick={() => generateNumber()} style={{marginLeft: '10px' }}>Roll</button>
+      <h2>Your Number is: {number}</h2>
     </div>
   );
 }
 
-export default App;
+export default App
